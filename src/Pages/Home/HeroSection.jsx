718addbecbbdac4f7f9801f89ffcb25c14a81150
imandrew26 <imandrew@umich.edu>
2024-08-15 11:27:32 -0400

    Initial commit

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,21 @@
+export default function HeroSection(){
+    return(
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <p className="section--title">Hi, I'm Andrew</p>
+                <h1 className="hero--section--title">
+                    <span className="hero--section--title--color">
+                        Aspiring
+                    </span>
+                    <br />
+                    Software Developer
+                </h1>
+                <p className="hero--section--description">
+                    blah blah blah
+                    <br />
+                    blah blah
+                </p>
+            </div>
+        </section>
+    );
+}
