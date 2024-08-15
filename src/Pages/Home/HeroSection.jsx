9c94056894d6c8bcece939405ec6e27349b45ab6
imandrew26 <imandrew@umich.edu>
2024-08-15 15:07:32 -0400

    landing screen complete

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -2,19 +2,25 @@ export default function HeroSection(){
     return(
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
-                <p className="section--title">Hi, I'm Andrew</p>
-                <h1 className="hero--section--title">
-                    <span className="hero--section--title--color">
+                <div className="hero--section--content">
+                    <p className="section--title">Hi, I'm Andrew</p>
+                    <h1 className="hero--section--title">
+                        {/* <span className="hero--section--title--color"> */}
                         Aspiring
-                    </span>
-                    <br />
-                    Software Developer
-                </h1>
-                <p className="hero--section--description">
-                    blah blah blah
-                    <br />
-                    blah blah
-                </p>
+                        {/* </span> */}
+                        <br />
+                        Software Engineer
+                    </h1>
+                    <p className="hero--section--description">
+                        I'm a student studying computer science + business
+                        <br />
+                        at the University of Michigan.
+                    </p>
+                </div>
+                <button className="btn btn-primary"> Get in Touch </button>
+            </div>
+            <div className = "hero--section--img">
+                <img src = "./img/headshot.png" alt = "Hero Headshot" />
             </div>
         </section>
     );
