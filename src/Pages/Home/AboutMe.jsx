6e29b45433e2f99fc5fd4bb490ee71b9ff2616c1
imandrew26 <imandrew@umich.edu>
2024-08-15 16:29:34 -0400

    About me complete

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,31 @@
+export default function AboutMe() {
+    return (
+      <section id="AboutMe" className="about--section">
+        <div className="about--section--img">
+          <img src="./img/about-me-fishing.png" alt="About Me" />
+        </div>
+        <div className="hero--section--content--box about--section--box">
+          <div className="hero--section--content">
+            <h1 className="skills-section--heading">About Me</h1>
+            <p className="hero--section--description">
+                Hi, my name is Andrew Wang — I'm a student at the University of Michigan
+                studying computer science and business. I enjoy using programming as a
+                creative outlet, where I can bring my ideas to life and solve problems.
+            </p>
+            <p className="hero--section--description">
+                In school, I'm involved in Michigan Data Science, Alternative Investments Club, 
+                and Phi Gamma Nu professional fraternity.
+            </p>
+            <p className="hero--section--description">
+                I spend my free time playing tennis and pickleball, discovering 
+                new music, fishing (pictured left), and hanging out with friends. I'm also very competitive when
+                it comes to video games.
+            </p>
+            <p className="hero--section--description">
+                Thanks for visiting my website!
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,9 +1,13 @@
 import HeroSection from "../HeroSection";
+import AboutMe from "../AboutMe";
+import MyPortfolio from "../MyPortfolio";
 
 export default function Home(){
     return(
         <>
             <HeroSection />
+            <AboutMe />
+            <MyPortfolio/>
         </>
     );
 }
